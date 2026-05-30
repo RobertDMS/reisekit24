000757f2b8163cfa22cc946c2741fa1a49604911
Robert Frąszczak <C:\ProgramData\The Bat!>
2026-05-30 21:00:49 +0200

    Opis zmiany

--- src/data/site.js
+++ src/data/site.js
@@ -2,6 +2,8 @@ export const site = {
   name: 'reisekit24',
   url: 'https://reisekit24.de',
   language: 'de',
+  // Set to false when the public website should be visible again.
+  underConstruction: true,
   description:
     'reisekit24 hilft Reisenden, Ausflüge, Hotels, eSIM und Pauschalreisen schnell zu finden und direkt beim Partner zu buchen.',
   logo: '/logo-reisekit24.png',
